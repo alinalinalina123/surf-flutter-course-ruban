281640510dc7472077cf3789aff9d263d22f9812
Alina Ruban <alina.padalianchyk@itechart-group.com>
2020-12-10 14:51:48 +0300

    Add app bar

--- places/lib/ui/screen/sight_list_screen.dart
+++ places/lib/ui/screen/sight_list_screen.dart
@@ -6,8 +6,30 @@ class SightListScreen extends StatefulWidget {
 }
 
 class _SightListScreenState extends State<SightListScreen> {
+  var appBarText = Padding(padding: EdgeInsets.only(left: 16, top: 64, right: 16),
+      child: Text(
+          "Список \nинтересных мест",
+          overflow: TextOverflow.ellipsis,
+          style: TextStyle(
+            color: Color(0xff3B3E5B),
+            fontFamily: "Roboto",
+            fontStyle: FontStyle.normal,
+            fontSize: 32,
+          ),
+      ),
+  );
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(150),
+        child: AppBar(
+            flexibleSpace: appBarText,
+            backgroundColor: Colors.transparent,
+            elevation: 0.0
+        ),
+      ),
+    );
   }
 }
