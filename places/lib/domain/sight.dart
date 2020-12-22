3d5c97e3d8f6211f934b06236368a49e2d7b8a54
Alina Ruban <alina.padalianchyk@itechart-group.com>
2020-12-22 15:31:11 +0300

    Add card widget

--- places/lib/domain/sight.dart
+++ places/lib/domain/sight.dart
@@ -1,13 +1,24 @@
 
 enum SightType {
-  building,
-  monument,
   museum,
-  statue,
   tower,
   bridge,
-  palace,
   other
+}
+
+extension SightTypeName on SightType {
+  String get name {
+    switch (this) {
+      case SightType.museum:
+        return 'Музей';
+      case SightType.tower:
+        return 'Башня';
+      case SightType.bridge:
+        return 'Мост';
+      default:
+        return "Неизвестный тип";
+    }
+  }
 }
 
 class Sight {
--- places/lib/ui/res/text_styles.dart
+++ places/lib/ui/res/text_styles.dart
@@ -1,6 +1,5 @@
-
+import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
-
 
 TextStyle appBarStyle = const TextStyle(
   color: Color(0xff3B3E5B),
@@ -9,4 +8,33 @@ TextStyle appBarStyle = const TextStyle(
   fontSize: 32,
 );
 
+TextStyle whiteTitleStyle = const TextStyle(
+  color: Colors.white,
+  fontFamily: "Roboto",
+  fontWeight: FontWeight.w700,
+  fontStyle: FontStyle.normal,
+  fontSize: 14,
+);
+
+TextStyle greyTitleStyle = const TextStyle(
+  color: Colors.black,
+  fontFamily: "Roboto",
+  fontWeight: FontWeight.w500,
+  fontStyle: FontStyle.normal,
+  fontSize: 16,
+);
+
+TextStyle greySubTitleStyle = const TextStyle(
+  color: Colors.grey,
+  fontFamily: "Roboto",
+  fontStyle: FontStyle.normal,
+  fontSize: 14,
+);
+
 EdgeInsets appBarPadding = const EdgeInsets.only(left: 16, top: 64, right: 16);
+EdgeInsets standardWidgetPadding = EdgeInsets.all(16);
+BorderRadius standardWidgetCircleBorder = BorderRadius.circular(16);
+BorderRadius standardWidgetTopCircleBorder = BorderRadius.only(
+  topLeft: Radius.circular(16),
+  topRight: Radius.circular(16),
+);
