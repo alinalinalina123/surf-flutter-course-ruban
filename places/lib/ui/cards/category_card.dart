38f32c253912a86b2766d650ea90b57a880c3e46
Alina Ruban <alina.padalianchyk@itechart-group.com>
2021-01-29 17:19:18 +0300

    Add ripple effect

--- places/lib/ui/cards/category_card.dart
+++ places/lib/ui/cards/category_card.dart
@@ -3,53 +3,61 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:places/domain/category.dart';
 import 'package:places/ui/res/assets_name.dart';
 import 'package:places/ui/res/colors.dart';
+import 'package:places/ui/res/text_styles.dart';
 
 //Class that provide widget for categories filter view
 class CategoryCard extends StatefulWidget {
   final Category category;
   final Function(Category) notifyParent;
 
-  CategoryCard({Key key, @required this.category, @required this.notifyParent}) : super(key: key);
+  CategoryCard({Key key, @required this.category, @required this.notifyParent})
+      : super(key: key);
 
   @override
   _CategoryCardState createState() => _CategoryCardState();
 }
 
 class _CategoryCardState extends State<CategoryCard> {
-
   @override
   Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Stack(
+          alignment: Alignment.bottomRight,
+          children: [
+            _buildCircleButton(),
+            if (widget.category.isSelected)
+              CircleAvatar(
+                radius: 12.0,
+                backgroundColor: colorLightGreen,
+                child: SvgPicture.asset(ticIcon),
+              ),
+          ],
+        ),
+        Text(widget.category.name)
+      ],
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisSize: MainAxisSize.max,
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    );
+  }
+
+  Widget _buildCircleButton() {
     return InkWell(
       splashColor: Colors.transparent,
+      customBorder: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(32),
+      ),
       onTap: () {
         setState(() {
           widget.category.isSelected = !widget.category.isSelected;
           widget.notifyParent(widget.category);
         });
       },
-      child: Column(
-        children: [
-          Stack(
-            alignment: Alignment.bottomRight,
-            children: [
-              CircleAvatar(
-                radius: 32.0,
-                backgroundColor: colorLightGreen,
-                child: SvgPicture.asset(widget.category.iconName),
-              ),
-              if (widget.category.isSelected)
-                CircleAvatar(
-                  radius: 12.0,
-                  backgroundColor: colorLightGreen,
-                  child: SvgPicture.asset(ticIcon),
-                ),
-            ],
-          ),
-          Text(widget.category.name)
-        ],
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisSize: MainAxisSize.max,
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      child: CircleAvatar(
+        radius: 32.0,
+        backgroundColor: colorLightGreen,
+        child: SvgPicture.asset(widget.category.iconName),
       ),
     );
   }
